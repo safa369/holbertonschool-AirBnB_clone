8a127633bd36d1c48e3050ef9c61121a1e1772e5
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-12-19 16:50:21 +0100

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -4,13 +4,14 @@
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel():
-    """a class base model that defines all common attributes/ methods of other classes
+    """a class base model that defines all common attributes/methods
     Attributes:
             id: the id of each basemodel (string).
             created_at: the date and time when the instance is created.
             updated_at: the date and time when the instance is apdated."""
-    
+
     def __init__(self):
         """assign the attributes"""
         self.id = str(uuid4())
@@ -19,15 +20,16 @@ class BaseModel():
 
     def __str__(self):
         """return the infrmation n human readable"""
-        return ("[{}] {} {}".format(self.__class__.__name__, self.id, self.__dict__))
-    
+        return ("[{}] {} {}"
+                .format(self.__class__.__name__, self.id, self.__dict__))
+
     def save(self):
         """save the current datetime of the updates """
         updated_at = datetime.now()
 
     def to_dict(self):
         """return a dictionary containig all key and value of the instance"""
-        dict_f ={}
+        dict_f = {}
         dict_f["__class__"] = self.__class__.__name__
         for key, value in self.__dict__.items():
             if isinstance(value, datetime):

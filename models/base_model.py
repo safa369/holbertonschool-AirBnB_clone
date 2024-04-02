e6aaf46bcbcd21f906e9960353697ad0368e5820
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-04-02 16:07:24 +0200

    models

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 import uuid
 from datetime import datetime
+from . import storage
 
 
 class BaseModel:
@@ -36,6 +37,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the object class"""
@@ -45,6 +47,7 @@ class BaseModel:
         """ Updates the public instance attribute update_at
         with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing key/value

1174b18cf6825a5699a801c80a3b36536340163b
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-04-02 14:28:40 +0200

    base models

--- models/base_model.py
+++ models/base_model.py
@@ -6,10 +6,41 @@ from datetime import datetime
 
 
 class BaseModel:
-    """This is the base model class    """
-    id = str(uuid.uuid4())
-    created_at = datetime.now()
-    updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """
+        Initialiazes new instance of BaseModel.
+
+        Args:
+            *args: Unused positional arguments
+            **kwargs: Dictionary representation of an instance.
+
+        If kwargs is not empty:
+            Each key has an attribute name
+            Each value is the value of the corresponding attr name
+            Convert datetime to datetime objects
+
+        Otherwise:
+            Create id and created_at values as initially done
+        """
+        if kwargs:
+            if '__class__' in kwargs:
+                # Remove '__class__' from the dictionary
+                del kwargs['__class__']
+            if 'created_at' in kwargs:
+                kwargs['created_at'] = datetime.strptime(
+                        kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if 'updated_at' in kwargs:
+                kwargs['updated_at'] = datetime.strptime(
+                        kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
 
     def __str__(self):
         """Returns a string representation of the object class"""

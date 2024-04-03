dcec218c976c22baeee6c3a85fc37ec36c98be00
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-04-03 17:40:53 +0200

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Class creation to define attrs/methods of the console"""
-from os import path
 import json
-from models.base_model import BaseModel
+import os
 
 
 class FileStorage:
@@ -14,22 +13,33 @@ class FileStorage:
 
     def all(self):
         """Returns a dictionaty '__objects'"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """Sets in '__objects' the object with the key """
-        key = self.__class__.__name__ + "." + obj.id
-        self.__objects[key] = obj
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """Serializes '__objects' dictionary to a JSON file"""
-        data = {key: value.to_dict() if isinstance(value, BaseModel) else value
-                for key, value in self.__objects.items()}
-        with open(self.__file_path, mode='w') as file:
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
+            data = {key: value.to_dict() for key,
+                    value in FileStorage.__objects.items()}
             json.dump(data, file)
 
     def reload(self):
         """Deserializes the JSON file to __objects' dictionary'"""
-        if path.exists(self.__file_path):
-            with open(self.__file_path, "r") as file:
-                self.__objects = json.loads(file.read())
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            obj_dict = {key: self.classes()[value["__class__"]](**value)
+                        for key, value in obj_dict.items()}
+            FileStorage.__objects = obj_dict
+
+    def classes(self):
+        """Returns a dictionary of valid classes and their references"""
+        from models.base_model import BaseModel
+
+        classes = {"BaseModel": BaseModel}
+        return classes
